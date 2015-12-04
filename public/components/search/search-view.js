1738380e55f42579f1ead61def9b5ade737a06af
Jacob Thousand <thousandjake@gmail.com>
2015-12-03 21:33:49 -0600

    prevents multiple requests for searchView template

--- public/components/search/search-view.js
+++ public/components/search/search-view.js
@@ -1,25 +1,30 @@
-var SearchView = function(search, tagName) {
+var SearchView = function (search, tagName) {
   this.search = search;
   this.tagName = tagName;
+  this.searchTemplate = '';
 };
 
-SearchView.prototype.render = function() {
-  var searchTemplate = "";
+SearchView.prototype.render = function () {
   var that = this;
+
   var templateReady = new Promise(
-    function(resolve, reject){
-      var xhr = new XMLHttpRequest();
-      xhr.addEventListener('load', function(response){
-        resolve(response.currentTarget.response);
-        searchTemplate = response.currentTarget.response;
-      });
-      xhr.addEventListener('error', function(){reject('fuckkk')});
-      xhr.open('GET','/components/search/search.html');
-      xhr.send();
-    }).catch(function(message){
-      console.log('promise caught');
+    function (resolve, reject) {
+      if (that.searchTemplate.length === 0) {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', function (response) {
+          resolve(response.currentTarget.response);
+          that.searchTemplate = response.currentTarget.response;
+        });
+        xhr.addEventListener('error', function () {reject('fuckkk')});
+        xhr.open('GET','/components/search/search.html');
+        xhr.send();
+      } else {
+        resolve(that.searchTemplate);
+      }
+
+    }).catch(function (message) {
       alert(message);
-    }).then(function(searchTemplate){
+    }).then(function (searchTemplate) {
       [].slice
         .call(document.getElementsByTagName(that.tagName))
         .forEach(function (currentSearchElement) {
