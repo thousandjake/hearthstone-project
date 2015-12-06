e1bfb65081b91c523c1de8542b14700752b1484a
Jacob Thousand <thousandjake@gmail.com>
2015-12-06 01:37:28 -0600

    added deck templating in deckView object

--- public/components/app.js
+++ public/components/app.js
@@ -2,7 +2,7 @@ var App = function() {
   this.search = new Search();
   this.deck = new Deck();
   this.searchView = new SearchView(this.search, 'Search');
-  this.deckView = new DeckView(this.deck);
+  this.deckView = new DeckView(this.deck, 'Deck');
 
   document.addEventListener('DOMContentLoaded',function(){
     //create a new search object and call the setup method with no inputs
--- public/components/deck/deck-view.js
+++ public/components/deck/deck-view.js
@@ -1,9 +1,35 @@
-var DeckView = function(deck) {
+var DeckView = function(deck, tagName) {
   this.deck = deck;
+  this.tagName = tagName;
+  this.deckTemplate = '';
 };
 
 DeckView.prototype.render = function() {
+  var that = this;
 
+  var templateReady = new Promise(
+    function (resolve, reject) {
+      if (that.deckTemplate.length === 0) {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', function (response) {
+          resolve(response.currentTarget.response);
+          that.deckTemplate = response.currentTarget.response;
+        });
+        xhr.addEventListener('error', function () {reject()});
+        xhr.open('GET','/components/deck/deck.html');
+        xhr.send();
+      } else {
+        resolve(that.deckTemplate);
+      }
+    }).catch(function () {
+      console.log('failed to get deck template from server');
+    }).then(function (deckTemplate) {
+      [].slice.call(document.getElementsByTagName(that.tagName))
+      //cast HTML collection returned by response to an array
+        .forEach(function (currentDeckElement) {
+          currentDeckElement.innerHTML = Mustache.render(deckTemplate,{});
+        });
+    });
 };
 
 DeckView.prototype.removeFromDeck = function() {
