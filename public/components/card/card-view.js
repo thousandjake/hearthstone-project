16193fa23271c25f6eb110746641d35bdfaa9dc1
Jacob Thousand <thousandjake@gmail.com>
2015-12-22 22:44:17 -0600

    updated mustache template rendering

--- public/components/card/card-view.js
+++ public/components/card/card-view.js
@@ -1,13 +1,10 @@
 var CardView = {
-  render : (function (cardData) {
+  render : function (cardData) {
     AppTemplateCache.getTemplate('/components/card/card.html')
       .then(function (cardTemplate) {
         var newCard = document.createElement('Card');
         document.getElementsByTagName('Results')[0].appendChild(newCard);
-        newCard.innerHTML = Mustache.render(cardTemplate,{
-          cardImage : cardData.img,
-          cardName : cardData.name
-        });
+        newCard.innerHTML = Mustache.render(cardTemplate, cardData);
         newCard.getElementsByClassName('add')[0]
           .addEventListener('click', function () {
             AppDispatcher.dispatch('add-card', cardData);
@@ -17,24 +14,18 @@ var CardView = {
             AppDispatcher.dispatch('view-card', cardData);
           });
       });
-  }).bind(CardView),
-  enlarge : (function (cardData) {
+  },
+  enlarge : function (cardData) {
     AppTemplateCache.getTemplate('/components/card/enlarged-card.html')
       .then(function (enlargedCardTemplate) {
         var overlay = document.createElement('Overlay');
         document.getElementById('main-container').appendChild(overlay);
-        overlay.innerHTML = Mustache.render(enlargedCardTemplate, {
-          cardImage : cardData.img,
-          cardName : cardData.name,
-          cardSet : cardData.cardSet,
-          cardFlavor : cardData.flavor,
-          cardArtist : cardData.artist
-        });
+        overlay.innerHTML = Mustache.render(enlargedCardTemplate, cardData);
         overlay.addEventListener('click', function () {
           document.getElementById('main-container').removeChild(overlay);
         });
       });
-  }).bind(CardView)
+  }
 };
 
 AppDispatcher.register('render-card', CardView.render);
