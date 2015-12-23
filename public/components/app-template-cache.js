6d044a9247d8b2238d3d88862a8aad6166dcfe83
Jacob Thousand <thousandjake@gmail.com>
2015-12-22 21:32:44 -0600

    refactored AppTemplateCache

--- public/components/app-template-cache.js
+++ public/components/app-template-cache.js
@@ -1,21 +1,21 @@
-var AppTemplateCache =  {
+var AppTemplateCache = {
   templates : {},
   getTemplate : function (HTMLpath) {
-    var that = this;
-    var templateReady = new Promise(function (resolve, reject) {
-        if(that.templates.hasOwnProperty(HTMLpath)) {
-          resolve(that.templates[HTMLpath]);
-        } else {
-          var xhr = new XMLHttpRequest();
-          xhr.addEventListener('load', function (response) {
-            that.templates[HTMLpath] = response.currentTarget.response;
-            resolve(that.templates[HTMLpath]);
-          });
-          xhr.addEventListener('error', function () {reject()});
-          xhr.open('GET', HTMLpath);
-          xhr.send();
-        };
+    return new Promise(function (resolve, reject) {
+      if(AppTemplateCache.templates.hasOwnProperty(HTMLpath)) {
+        resolve(AppTemplateCache.templates[HTMLpath]);
+      } else {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', function (response) {
+          AppTemplateCache.templates[HTMLpath] = response.currentTarget.response;
+          resolve(AppTemplateCache.templates[HTMLpath]);
+        });
+        xhr.addEventListener('error', function () {
+          reject();
+        });
+        xhr.open('GET', HTMLpath);
+        xhr.send();
+      }
     });
-    return templateReady;
   }
 };
