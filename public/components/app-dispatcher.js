943c551dac360047e7572ab226715773e5ebe740
Jacob Thousand <thousandjake@gmail.com>
2015-12-22 21:22:22 -0600

    refactored AppDispatcher

--- public/components/app-dispatcher.js
+++ public/components/app-dispatcher.js
@@ -1,36 +1,25 @@
 var AppDispatcher = {
   listeners : {},
   register : function (eventName, callback) {
-    var that = this, eventFound = 'false';
-    for(var key in that.listeners) {
-      if(eventName === key) {
-        eventFound = 'true';
-      };
-    };
-    if(eventFound === 'true') {
-      that.listeners[eventName].push(callback);
+    if(this.listeners.hasOwnProperty(eventName)) {
+      this.listeners[eventName].push(callback);
     } else {
-      that.listeners[eventName] = [callback];
-    };
+      this.listeners[eventName] = [callback];
+    }
   },
   dispatch : function (eventName, args) {
-    var that = this, eventFound = 'false';
-    for(var key in that.listeners) {
-      if(eventName === key) {
-        eventFound = 'true';
-      };
-    };
-    if(eventFound === 'true'){
-      that.listeners[eventName].forEach(function (currentValue) {
+    if(this.listeners.hasOwnProperty(eventName)){
+      this.listeners[eventName].forEach(function (currentValue) {
         currentValue(args);
-      }, that);
+      });
     } else {
-      console.error('Event undefined.  Event: '
-        + eventName
-        + ' with Arguments: '
-        + args
-        + ' is not a registered event'
+      console.error(
+        'Event undefined.  Event:',
+        eventName,
+        'with Arguments:',
+        args,
+        'is not a registered event'
       );
-    };
+    }
   }
 };
